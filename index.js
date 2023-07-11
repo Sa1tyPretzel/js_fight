82827e4b7530e550c7a9afc92fb8e6fdfa458d52
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-07-11 15:39:32 +0530

    add attack collision detection

--- index.js
+++ index.js
@@ -9,16 +9,32 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.6
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = "yellow"}) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
+        this.color = color
     }
 
     draw() {
-        c.fillStyle = 'green'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attack box
+        c.fillStyle = "blue"
+        c.fillRect(
+            this.attackBox.position.x, 
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height
+        )
     }
 
     update () {
@@ -45,8 +61,6 @@ const player = new Sprite({
     }
 })
 
-
-
 const enemy = new Sprite({
     position: {
     x: 400,
@@ -55,7 +69,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: "pink" 
 })
 
 enemy.draw();
@@ -104,7 +119,16 @@ function animate() {
     }
      else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
         enemy.velocity.x = 5
-     }
+    }
+
+    //detect for collision
+    if (player.attackBox.position.x + player.attackBox.width >= 
+        enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width &&
+        player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
+        player.attackBox.position.y <= enemy.position.y + enemy.height
+        ) {
+        console.log("lol");
+    }
 }
 
 animate()
