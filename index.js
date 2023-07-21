03b461979660cb3052905ee7b5b900d6475d3370
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-07-21 23:36:00 +0530

    add game over

--- index.js
+++ index.js
@@ -129,12 +129,28 @@ function rectangularCollision({rectangle1, rectangle2}) {
     )
 }
 
-let timer = 10
+function determineWinner({player, enemy}) {
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }   else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    }   else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
+let timer = 60
 function decreaseTimer() {
-    setTimeout(decreaseTimer, 1000)
     if (timer > 0) {
+        setTimeout(decreaseTimer, 1000)
         timer--
         document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0) {
+        
+        determineWinner({player, enemy})
     }
 }
 
@@ -189,6 +205,11 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + "%"
+    }
+
+    //end game on health 
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({player, enemy})
     }
 }
 
