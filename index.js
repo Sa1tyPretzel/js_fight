cf0c56dad924a951fa64567d935e35f2e1a7151c
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-07-21 23:57:31 +0530

    add timer halt

--- index.js
+++ index.js
@@ -129,7 +129,8 @@ function rectangularCollision({rectangle1, rectangle2}) {
     )
 }
 
-function determineWinner({player, enemy}) {
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
     document.querySelector('#displayText').style.display = 'flex'
     if (player.health === enemy.health) {
         document.querySelector('#displayText').innerHTML = 'Tie'
@@ -141,16 +142,16 @@ function determineWinner({player, enemy}) {
 }
 
 let timer = 60
+let timerId
 function decreaseTimer() {
     if (timer > 0) {
-        setTimeout(decreaseTimer, 1000)
+        timerId = setTimeout(decreaseTimer, 1000)
         timer--
         document.querySelector('#timer').innerHTML = timer
     }
 
     if (timer === 0) {
-        
-        determineWinner({player, enemy})
+        determineWinner({player, enemy, timerId})
     }
 }
 
@@ -209,7 +210,7 @@ function animate() {
 
     //end game on health 
     if (enemy.health <= 0 || player.health <= 0) {
-        determineWinner({player, enemy})
+        determineWinner({player, enemy, timerId})
     }
 }
 
