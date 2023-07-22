2fca2ea3bcfd38b0a30241dffa1598db4984b953
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-07-22 23:36:44 +0530

    refactor classes

---
+++ js/classes.js
@@ -0,0 +1,72 @@
+class Sprite {
+    constructor({position}) {
+        this.position = position
+        this.width = 50
+        this.height = 150
+        }
+
+    draw() {}
+
+    update () {
+       this.draw()
+    }
+}
+
+class Fighter {
+    constructor({position, velocity, color = "orange", offset}) {
+        this.position = position
+        this.velocity = velocity
+        this.width = 50
+        this.height = 150
+        this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset : offset,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking
+        this.health = 100
+    }
+
+    draw() {
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attack box
+        if (this.isAttacking) {
+            c.fillStyle = "blue"
+            c.fillRect(
+              this.attackBox.position.x, 
+              this.attackBox.position.y,
+              this.attackBox.width,
+              this.attackBox.height
+            )
+        }  
+    }
+
+    update () {
+       this.draw()
+       this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+       this.attackBox.position.y = this.position.y
+       
+       this.position.x += this.velocity.x
+       this.position.y += this.velocity.y
+
+       if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+          this.velocity.y = 0  
+       } else
+       this.velocity.y += gravity
+    }
+
+    attack() {
+       this.isAttacking = true
+       setTimeout(() => {
+         this.isAttacking = false       
+       }, 100) 
+    }
+}
